dc65312f30de6090dc20ec4be43239cfd618cefa
Travis Beatty <travisby@gmail.com>
2013-04-30 11:33:53 -0400

    Finished the CRUD functions for the main Model

--- models/model.php
+++ models/model.php
@@ -4,6 +4,11 @@ class Model {
 
     protected $id = 0;
 
+    // Create
+    protected static function create($arr_fields) {
+    }
+
+    // Read
     protected static function getByID($id) {
         $this->getBy(
             array('id' => $id),
@@ -14,6 +19,19 @@ class Model {
     protected static function getBy($arr_fields, $limit=0) {
     }
 
+    // Update
+    protected static function updateBy($arr_fields_matching, $arr_fields_update, $limit=0) {
+    }
+
+    protected static function updateByID($id, $arr_fields_update) {
+        $this->updateBy(
+            array('id' => $id),
+            $arr_fields_update,
+            $limit=1
+        )
+    }
+
+    // Destroy
     protected static function deleteByID($id) {
         $this->deleteBy(
             array('id' => $id),
