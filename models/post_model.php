a0deca4dd7241a9b28e061c734e086694cb9214f
Travis Beatty <travisby@gmail.com>
2013-04-30 18:59:37 -0400

    Added the static fields to the post model

--- models/post_model.php
+++ models/post_model.php
@@ -3,6 +3,6 @@ include "model.php";
 
 class Post_Model extends Model {
     protected static $table = 'Posts';
-    protected static $fields = array();
+    protected static $fields = array('postID', 'topicID', 'message');
 }
 ?>
