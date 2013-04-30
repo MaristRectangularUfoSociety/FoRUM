87c7cbbe40d5da094f6fb632e6ac3892bf1c2533
Travis Beatty <travisby@gmail.com>
2013-04-30 18:59:31 -0400

    Added the static fields to the forum model

--- models/forum_model.php
+++ models/forum_model.php
@@ -3,6 +3,6 @@ include "model.php";
 
 class Forum_Model extends Model {
     protected static $table = 'Forums';
-    protected static $fields = array();
+    protected static $fields = array('forumID', 'categoryID', 'order', 'name');
 }
 ?>
