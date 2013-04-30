354efd718b05f88750b2890d2fed6b2a510820c8
Travis Beatty <travisby@gmail.com>
2013-04-30 11:28:51 -0400

    Changed our delete and get functions to call their similar functions

--- models/model.php
+++ models/model.php
@@ -3,12 +3,20 @@ class Model {
     protected static $table = '';
 
     protected static function getByID($id) {
+        $this->getBy(
+            array('id' => $id),
+            $limit=1
+        );
     }
 
     protected static function getBy($arr_fields, $limit=0) {
     }
 
     protected static function deleteByID($id) {
+        $this->deleteBy(
+            array('id' => $id),
+            $limit=1
+        );
     }
 
     protected static function deleteBy($arr_fields, $limit=0) {
