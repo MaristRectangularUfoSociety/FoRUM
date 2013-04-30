c0d166140729a0e39f049f943fcee18777ce5e53
Travis Beatty <travisby@gmail.com>
2013-04-30 10:55:18 -0400

    Added some basic functions to our Model

--- models/model.php
+++ models/model.php
@@ -1,4 +1,17 @@
 <?php
 class Model {
+
+    public static function getByID($id) {
+    }
+
+    public static function getBy($arr_fields, $limit=0) {
+    }
+
+    public static function deleteByID($id) {
+    }
+
+    public static function deleteBy($arr_fields, $limit=0) {
+    }
+
 }
 ?>
