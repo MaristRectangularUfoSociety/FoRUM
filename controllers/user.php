929c13c15b0c4b427d308129460b63d48e1b3379
Travis Beatty <travisby@gmail.com>
2013-05-13 22:21:37 -0400

    Refactored users controller

--- controllers/user.php
+++ controllers/user.php
@@ -1,26 +1,17 @@
 <?php
-class Users{
-//done(ish)
-	function list{
-		$users = getmealltheusers;
-		$data = array('users'=> $users);
-		loadview(nameofnicksview,$data);
-	}
-
-	function create{
-		if(isset($_GET['var'])){
-			loadview(Register.php, array());
-		}
-		elseif(isset($_POST)){
-			$user = $_POST['username']
-			$user = $_POST['email']
-			$user = $_POST['password']
-
-			travis.createthisuser($user);
-			index.index();
-		}
-
-
-	}
+class User extends Controller {
+    function register() {
+        if (!empty($_POST)) {
+            $this->user_model->create(
+                array(
+                    'username' => $_POST['username'],
+                    'email' => $_POST['email'],
+                    'password' => $_POST['password']
+                )
+            );
+        } else {
+            $this->loadView('register', array());
+        }
+    }
 }
 ?>
