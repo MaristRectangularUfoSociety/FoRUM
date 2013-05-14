69a6685f411b682c62f96ed535a80d53fb23dbce
Travis Beatty <travisby@gmail.com>
2013-05-13 21:43:33 -0400

    Loaded all models in our default controller

--- controller.php
+++ controller.php
@@ -6,6 +6,12 @@ class Controller {
 
     public function __construct($name) {
         $this->name = $name;
+        $this->audit_model = new Audit_model();
+        $this->category_model = new Category_model();
+        $this->forum_model = new Forum_model();
+        $this->post_model = new Post_model();
+        $this->topic_model = new Topic_model();
+        $this->user_model = new User_model();
     }
 
     public function loadView($view_location, $data) {
