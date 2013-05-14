10836d322f3f228a821e6e9ba27553dc30ea4b6b
Travis Beatty <travisby@gmail.com>
2013-05-13 22:19:40 -0400

    Refactored topics controller

--- controllers/topic.php
+++ controllers/topic.php
@@ -1,29 +1,19 @@
 <?php
-class Topic{
-//done(ish)
-	function create{
-		if(isset($_GET['var'])){
-			loadview(nameofnicksview, array());
-		}
-		elseif(isset($_POST)){
-			$topic = $_POST['catagory']
-			$topic = $_POST['username']
-			$topic = $_POST['text']
-			$topic = $_POST['timestamp']
+class Topic extends Controller {
 
-			travis.createthisuser($topic);
-			index.index();
-		}
-
-	function list {
-	$post = getMeAllTheCategories();
-
-	$data = array('post' => $post);
-
-		loadView('nicksViewThatIWantToLoad', $post);
-
-	}
-
-	}
+    function create() {
+        if (!empty($_POST)) {
+            $this->topic_model->create(
+                array(
+                    'category' => $_POST['category'],
+                    'user' => $_POST['user'],
+                    'text' => $_POST['text']
+                )
+            );
+        } else {
+            $this->loadView('create_topic', array());
+        }
+    }
 }
 ?>
+
