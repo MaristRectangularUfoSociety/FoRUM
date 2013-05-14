7440e12247ab03bb97bcf82e5b3ac5b3affc26eb
Travis Beatty <travisby@gmail.com>
2013-05-13 22:17:18 -0400

    Refactored post controller

--- controllers/post.php
+++ controllers/post.php
@@ -1,19 +1,15 @@
-<?php
-class Post{
-//done(ish)
-	function create{
-		if(isset($_GET['var'])){
-			loadview(nameofnicksview, array());
-		}
-		elseif(isset($_POST)){
-			$post = $_POST['topic']
-			$post = $_POST['username']
-			$post = $_POST['text']
-			$post = $_POST['timestamp']
-
-			travis.createthistopic($post);
-			index.index();
-		}
-
+class Post extends Controller {
+    function create() {
+        if (!empty($_POST)) {
+            $this->post->create(
+                array(
+                    'topic' => $_POST['topic'],
+                    'user' => $_POST['user'],
+                    'text' => $_POST['text'],
+                )
+            );
+        } else {
+            $this->loadView('create_post', array());
+    }
 }
 ?>
