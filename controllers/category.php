87eec3c8325a2a73d7d8717a52c30d86be5212e6
Travis Beatty <travisby@gmail.com>
2013-05-15 14:02:44 -0400

    Added a whitespace fix

--- controllers/category.php
+++ controllers/category.php
@@ -1,14 +1,14 @@
 <?php
 class Category extends Controller {
 
-	function loadCreateCategory() {
-		if (!empty($_POST)) {
+    function loadCreateCategory() {
+        if (!empty($_POST)) {
             $category = new Category_Model();
             $category->name = $_POST['name'];
             $category->save();
-		} else {
-		    $this->loadView('create_category', array());
-		}
-	}
+        } else {
+            $this->loadView('create_category', array());
+        }
+    }
 }
 ?>
