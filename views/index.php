fc9f11d16781d7eb79d169cca913dd4955141fa6
Nicholas La Roux <larouxn@gmail.com>
2013-05-15 18:05:50 -0400

    Converted all views to use seperate header and footer files

--- views/index.php
+++ views/index.php
@@ -1,40 +1,6 @@
 <?php
-
-session_start();
-
+	require_once('header.php');
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>FoRUM</title>
-    <meta content="Nicholas La Roux, Matthew Webber, Travis Beatty" name="author">
-    <meta content="Forum of Rectangular Ufo Messages" name="description">
-    <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-    <link rel="stylesheet" type="text/css" href="styles/main.css">
-</head>
-<body>
-	<div class="container">
-		<header>
-			<span class="left">
-				<ul>
-					<li><h1><a href="index.html">FoRUM</a></h1></li>
-					<li><a href="#">News</a></li>
-					<li>|</li>
-					<li><a href="">Forum</a></li>
-					<li>|</li>
-					<li><a href="#">Help</a></li>
-				</ul>
-			</span>
-			<span class="right login">
-				<form action="../scripts/loginUser.php" method="POST">
-					<input type="text" name="usernmame" placeholder="username"></input>
-					<input type="password" name="password" placeholder="password"></input>
-					<input type="submit" />
-				</form>
-			</span>
-		</header>
 		
 		<span class="content">
 			<hr>
@@ -57,12 +23,6 @@ session_start();
 			</span>
 		</span>
 		
-		<footer>
-		    <br>
-		    <hr>
-			&#169;2013 Marist Rectangular Ufo Society
-		</footer>
-		
-	</div>
-</body>
-</html>
+<?php
+	require_once('footer.php');
+?>
--- views/topic.php
+++ views/topic.php
@@ -1,38 +1,6 @@
 <?php
-
-session_start();
-
+	require_once('header.php');
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>FoRUM</title>
-    <meta content="Nicholas La Roux, Matthew Webber, Travis Beatty" name="author">
-    <meta content="Forum of Rectangular Ufo Messages" name="description">
-    <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-    <link rel="stylesheet" type="text/css" href="styles/main.css">
-</head>
-<body>
-	<div class="container">
-		<header>
-			<span class="left">
-				<ul>
-					<li><h1><a href="index.html">FoRUM</a></h1></li>
-					<li><a href="#">News</a></li>
-					<li>|</li>
-					<li><a href="">Forum</a></li>
-					<li>|</li>
-					<li><a href="#">Help</a></li>
-				</ul>
-			</span>
-			<span class="right login">
-				<input type="text" placeholder="username"></input>
-				<input type="password" placeholder="password"></input>
-				<button>Log In</button>
-			</span>
-		</header
 		
 		<span class="content">
 			<hr>
@@ -59,12 +27,6 @@ session_start();
 			</span>
 		</span>
 		
-		<footer>
-		    <br>
-		    <hr>
-			&#169;2013 Marist Rectangular Ufo Society
-		</footer>
-		
-	</div>
-</body>
-</html>
+<?php
+	require_once('footer.php');
+?>
