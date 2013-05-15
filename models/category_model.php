26de0a87018424db216a77a48e64d23ac3916f70
Travis Beatty <travisby@gmail.com>
2013-05-15 14:15:33 -0400

    Overloaded the category->getBy to include topics

--- models/category_model.php
+++ models/category_model.php
@@ -12,6 +12,16 @@ class Category_Model extends Model {
     function __construct() {
         parent::__construct();
     }
+
+    protected static function getBy($arr_fields, $limit=0) {
+        $category = parent::getBy($arr_fields, $limit);
+        $category->forums = $forum_model->getBy(
+            array(
+                'categoryID' => $this->categoryID
+            )
+        );
+        return $category;
+    }
 }
 
 $category_model = new Category_Model();
