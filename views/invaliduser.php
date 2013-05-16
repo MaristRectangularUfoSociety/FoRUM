5e1ac5a764847e451bfe6f738fbe9113282f2855
Travis Beatty <travisby@gmail.com>
2013-05-16 15:13:12 -0400

    Added invalid user view

---
+++ views/invaliduser.php
@@ -0,0 +1,14 @@
+<?php
+	require_once('header.php');
+?>
+		
+<span class="content">
+	<hr>
+	<span class="leftBody">
+	    Sorry, that user/password combination was invalid.  Please try again.
+	</span>
+</span>
+		
+<?php
+	require_once('footer.php');
+?>
