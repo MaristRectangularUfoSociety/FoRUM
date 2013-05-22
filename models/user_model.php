15d5812ae38a0e8ff321d766f2226ae761ee50cf
Travis Beatty <travisby@gmail.com>
2013-05-22 07:34:33 -0400

    modernized the user model

--- models/user_model.php
+++ models/user_model.php
@@ -1,16 +1,48 @@
 <?php
 class User_Model extends Model {
-    private $username = '';
-    private $passwordHashed = '';
+
+    public $username = '';
+    public $password = '';
+
+    protected $table = 'Users';
+    protected $fields = array('username', 'password');
+    protected $idField = 'userID';
+
+    protected $userID = 0;
 
     public function __construct() {
         parent::__construct();
-        $this->table = 'Users';
-        $this->fields = array();
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
     }
 
     public static function hashPassword($pass) {
         return $pass;
+    }
+
+    public function login($username, $password) {
+        $user = $this->getBy(
+            array(
+                'username' => $username,
+                'password' => $password
+            )
+        );
+
+        if ($user) {
+            $this->username = $user->getUsername();
+            $this->password = $user->getPassword();
+            $this->userID = $user->getID();
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
