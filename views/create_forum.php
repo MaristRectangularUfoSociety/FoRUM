afd709692b4dcaa1b739ec6ef2b51b3d6b612516
Travis Beatty <travisby@gmail.com>
2013-05-22 08:24:23 -0400

    Added forum create page

---
+++ views/create_forum.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form action="/index.php?page=create_forum" method="post">
+            categoryID: <input type="text" name="categoryID">
+            Name: <input type="text" name="name">
+            <input type="submit">
+        </form>
+    </body>
+</html>
